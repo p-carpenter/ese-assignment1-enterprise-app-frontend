075bfc806731b827c3b840b92be32c12c3a2b637
Poppy Carpenter <poppy.carpenter@ada.ac.uk>
2026-02-19 21:08:01 +0000

    refactor: create generic component button, refactor auth pages and forms

---
+++ src/components/layout/Button.tsx
@@ -0,0 +1,30 @@
+import { type ComponentPropsWithoutRef, type FC } from 'react';
+import styles from './Button.module.css';
+
+interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
+  variant?: 'primary' | 'outlined';
+  size?: 'small' | 'large';
+}
+
+const Button: FC<ButtonProps> = ({ 
+  variant = 'outlined', 
+  size = 'small', 
+  className = '', 
+  children, 
+  ...props 
+}) => {
+  const buttonClasses = [
+    styles.button,
+    styles[variant],
+    styles[size],
+    className
+  ].filter(Boolean).join(' ');
+
+  return (
+    <button className={buttonClasses} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
