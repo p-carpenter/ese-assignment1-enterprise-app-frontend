f756e1d68447e5910ec49b9d95c387df4e13169a
Poppy Carpenter <poppy.carpenter@ada.ac.uk>
2026-03-14 23:41:46 +0000

    chore: remove LoadingAnimation

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -1,3 +1,2 @@
-export { BubbleLoading as LoadingAnimation } from "./LoadingAnimation";
 export { MoreHorizontalOutline } from "./MoreHorizontalOutline";
 export { PencilSolid } from "./PencilSolid";
