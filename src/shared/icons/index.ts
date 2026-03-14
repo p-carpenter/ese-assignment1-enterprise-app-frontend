dcd642ddfe39a56d3d21ab49d8f206f00db4068f
Poppy Carpenter <poppy.carpenter@ada.ac.uk>
2026-03-14 23:35:17 +0000

    chore: delete unused icons

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -1,7 +1,3 @@
-export { ButtonNextSolid } from "./ButtonNextSolid";
-export { ButtonPause2Solid } from "./ButtonPause2Solid";
-export { ButtonPlaySolid } from "./ButtonPlaySolid";
-export { ButtonPreviousSolid } from "./ButtonPreviousSolid";
 export { BubbleLoading as LoadingAnimation } from "./LoadingAnimation";
 export { MoreHorizontalOutline } from "./MoreHorizontalOutline";
 export { PencilSolid } from "./PencilSolid";
