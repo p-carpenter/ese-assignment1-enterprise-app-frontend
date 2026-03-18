649adaf726d4d1d1db788c854c7575a9ce299fa2
Poppy Carpenter <poppy.carpenter@ada.ac.uk>
2026-03-18 15:43:07 +0000

    style(format): autoformat

--- src/features/songs/components/SongManagementDropdown/SongManagementDropdown.tsx
+++ src/features/songs/components/SongManagementDropdown/SongManagementDropdown.tsx
@@ -1,5 +1,11 @@
 import { type FC } from "react";
-import { Button, Menu, MenuItem, MenuTrigger, Popover } from "react-aria-components";
+import {
+  Button,
+  Menu,
+  MenuItem,
+  MenuTrigger,
+  Popover,
+} from "react-aria-components";
 import { MoreHorizontalOutline } from "@/shared/icons";
 import styles from "./SongManagementDropdown.module.css";
 
@@ -21,7 +27,7 @@ export const SongManagementDropdown: FC<SongManagementDropdownProps> = ({
       <Button className={styles.button}>
         <MoreHorizontalOutline />
       </Button>
-      
+
       <Popover placement="bottom end">
         <Menu className={styles.dropdownMenu}>
           {dropdownItems?.map((item) => (
